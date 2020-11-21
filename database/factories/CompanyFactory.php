4784e0931ade027eeefcf3d551a6ad50bba13646
Obinna <obinna@fivenines.com.ng>
2020-11-22 00:38:59 +0100

    write company factory

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -4,9 +4,14 @@
 
 use App\Company;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 
 $factory->define(Company::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->company,
+        'email' => $faker->companyEmail,
+        'password' => Hash::make('remember'),
+        'logo' => $faker->imageUrl($width = 150, $height = 48),
+        'url' => $faker->url
     ];
 });
