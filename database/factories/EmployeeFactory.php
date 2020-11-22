7e507af904bb277b05743497e1b1606006d12368
Obinna <obinna@fivenines.com.ng>
2020-11-22 01:02:49 +0100

    edit employee factory

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -4,9 +4,13 @@
 
 use App\Employee;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 
 $factory->define(Employee::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'email' => $faker->unique()->email,
+        'password' => Hash::make('employee'),
+        'company_id' => mt_rand(1, 20)
     ];
 });
