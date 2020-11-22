9ae82bc5fe7286705d983586f22cca8b8c19eec8
Obinna Okafor <obinna@fiveninesng.com>
2020-11-22 21:27:08 +0100

    define user relationship on employee model

--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    /**
+     * Get the employee level user associated with the employee.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
