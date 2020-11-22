41b56aaf631b2dd0e3ed97763f81d09f3bf2f954
Obinna <obinna@fivenines.com.ng>
2020-11-22 01:01:26 +0100

    employee migration structure

--- database/migrations/2020_11_21_225755_create_employees_table.php
+++ database/migrations/2020_11_21_225755_create_employees_table.php
@@ -15,6 +15,10 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('company');
             $table->timestamps();
         });
     }
