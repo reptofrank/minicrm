415656afc7091ca454631754a4dedda06524816d
Obinna <obinna@fivenines.com.ng>
2020-11-22 08:30:49 +0100

    define company foreign key on employee table migration

--- database/migrations/2020_11_21_225755_create_employees_table.php
+++ database/migrations/2020_11_21_225755_create_employees_table.php
@@ -18,7 +18,7 @@ class CreateEmployeesTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->integer('company');
+            $table->foreign('company_id')->references('id')->on('companies');
             $table->timestamps();
         });
     }
