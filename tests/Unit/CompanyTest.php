b4dfcfd14866b42ed2df4c0cc9e331896e5865ef
Obinna <obinna@fivenines.com.ng>
2020-11-22 11:11:52 +0100

    create unit test class for company and employee

---
+++ tests/Unit/CompanyTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\TestCase;
+
+class CompanyTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/EmployeeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\TestCase;
+
+class EmployeeTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
