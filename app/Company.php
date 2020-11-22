1899d4f1155a64eaee1a8e5ff79ee90f0c5852d6
Obinna Okafor <obinna@fiveninesng.com>
2020-11-22 21:26:30 +0100

    define user relationship on company model

--- app/Company.php
+++ app/Company.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    /**
+     * Get the company level user associated with the company.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
