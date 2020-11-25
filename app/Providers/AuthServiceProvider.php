734387a720237a600f79c30ecd9fe9aa62e46a44
Obinna <obinna@fivenines.com.ng>
2020-11-25 22:11:31 +0100

    admin grant all before gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        Gate::before(function($user){
+            return $user->role === 'admin' ? true : null;
+        });
+
         Gate::define('view-employees', function($user) {
             return $user->role === 'company';
         });
