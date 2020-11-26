44f24e72280eebee1dd12e6142610f0ed2b85b05
Obinna <obinna@fivenines.com.ng>
2020-11-26 15:08:24 +0100

    get users test

--- tests/Unit/AdminTest.php
+++ tests/Unit/AdminTest.php
@@ -9,5 +9,24 @@ class AdminTest extends TestCase
 {
     use RefreshDatabase;
 
-    
+    /**
+     * Test Get all users 
+     */
+    public function testViewAllUsers()
+    {
+        $users = factory(\App\Company::class, 3)->create()->each(function($company){
+            $company->employees()->createMany(factory(\App\Employee::class, 5)->make()->toArray());
+        });
+
+        $adminUser = factory(\App\User::class)->create();
+
+        $this->be($adminUser);
+
+        $response = $this->get('/users');
+
+        $data = $response->json();
+
+        $response->assertOk();
+        $this->assertCount(16, $data);
+    }
 }
