29486935631949122a33de530978a5c0038863f7
Obinna <obinna@fivenines.com.ng>
2020-11-26 13:39:51 +0100

    fix get companies with email test

--- tests/Unit/GuestTest.php
+++ tests/Unit/GuestTest.php
@@ -22,7 +22,26 @@ class GuestTest extends TestCase
 
         $data = $response->json();
 
+        $response->assertOk();
+
         $this->assertCount(5, $data['data']);
+    }
+
+    /**
+     * Test get all companies as guest with their email address.
+     *
+     * @return void
+     */
+    public function testGetAllCompaniesWithEmail()
+    {
+        factory(\App\Company::class, 5)->create();
+
+        $response = $this->get('/companies');
+
+        $data = $response->json();
+
+        $this->assertCount(5, $data['data']);
+        $this->assertArrayHasKey('email', $data['data'][0]);
     }
 
     /**
@@ -37,6 +56,8 @@ class GuestTest extends TestCase
         $response = $this->get('/companies');
 
         $data = $response->json();
+
+        $response->assertOk();
 
         $this->assertCount(5, $data['data']);
     }
