53994f63545a748c5707d117fbf53d5b08913f70
Obinna <obinna@fivenines.com.ng>
2020-11-26 15:10:04 +0100

    get users admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    /**
+     * Get all users
+     */
+    public function users(Request $request)
+    {
+        return response()->json(User::all());
+    }
 }
