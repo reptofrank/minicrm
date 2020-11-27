087e62c29cc8e5e72b095b2d7a9d65cfca55a37f
Obinna <obinna@fivenines.com.ng>
2020-11-27 18:18:12 +0100

    use home controller index for homepage

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
-
-Route::get('/', 'CompanyController@index');
 
 Route::middleware('auth')->group(function(){
     Route::resource('companies', CompanyController::class)->except('index');
