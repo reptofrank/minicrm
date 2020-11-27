1d5f4c8c558d5e4b7eb13f50dce59304b8c3d51f
Obinna <obinna@fivenines.com.ng>
2020-11-27 15:35:50 +0100

    fix layout

---
+++ resources/js/components/Companies.vue
@@ -0,0 +1,24 @@
+<template>
+    <table class="table table-hovered">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>URL</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <a href="#">Name</a>
+                </td>
+                <td>
+                    <a href="#">Email</a>
+                </td>
+                <td>
+                    <a href="#">Url</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
