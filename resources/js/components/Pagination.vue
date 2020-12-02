8c19e2bd62e92bb2d0d32ecef861b18145cce914
Obinna Okafor <obinna@fiveninesng.com>
2020-12-02 09:46:20 +0100

    pagination component

---
+++ resources/js/components/Pagination.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <li v-bind:class="{'page-item': true, disabled: !prev}">
+                <a href="#" v-on:click.prevent="$emit('previous-page')" class="page-link">Previous</a>
+            </li>
+            
+            <li v-bind:class="{'page-item': true, disabled: !next}">
+                <a href="#" v-on:click.prevent="$emit('next-page')" class="page-link">Next</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    props: ['prev', 'next', 'current_page']
+}
+</script>
