fb0721a6964f1e693c7fc34bbc3437da0fb31d78
Obinna Okafor <obinna@fiveninesng.com>
2020-12-02 01:36:08 +0100

    delete user

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,22 +1,23 @@
 <template>
-    <table class="table table-hovered">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="user in users" :key="user.name" v-bind:data-id="user.id">
-                <td>{{ user.name }}</td>
-                <td>{{ user.email }}</td>
-                <td>{{ user.role }}</td>
-                <td><button v-on:click="deleteUser">Delete</button></td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="container-fluid">
+        <h3>Users</h3>
+        <table class="table table-hovered">
+            <thead>
+                <tr>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in users" :key="user.email" v-bind:data-id="user.id">
+                    <td>{{ user.email }}</td>
+                    <td>{{ user.role }}</td>
+                    <td><button v-on:click="deleteUser" class="btn btn-danger">Delete</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
@@ -30,17 +31,22 @@ export default {
     },
     async created() {
         const response = await axios.get('/api/admin/users')
-        this.users = response.data.data
+        console.log(response)
+        this.users = response.data
     },
 
     methods: {
         async deleteUser(e) {
-            console.log(e.target.closest('tr').dataset.id)
-            // const res = confirm('Are you sure you want to delete this user?')
-            // if(res) {
-            //     const response = await axios.delete(`/api/admin/users/${id}`)
-            //     if(response.status === 204) alert('user deleted successfully')
-            // }
+            const row = e.target.closest('tr');
+            const id = row.dataset.id
+            const res = confirm('Are you sure you want to delete this user?')
+            if(res) {
+                const response = await axios.delete(`/api/admin/users/${id}`)
+                if(response.status === 204) {
+                    row.style.display = 'none';
+                    alert('user deleted successfully')
+                }
+            }
         }
     }
 }
