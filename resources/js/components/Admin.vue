922fbeb9e3ab11c02518f9d77466822f919d435e
Obinna Okafor <obinna@fiveninesng.com>
2020-12-02 01:34:05 +0100

    logout link

--- resources/js/components/Admin.vue
+++ resources/js/components/Admin.vue
@@ -12,7 +12,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="/logout">Logout</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
